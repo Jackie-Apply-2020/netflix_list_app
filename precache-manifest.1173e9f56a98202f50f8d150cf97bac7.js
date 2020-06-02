1407e925b7ef44767513dbacfade145d1aac7aa6
Jackie <jackielireact@gmail.com>
2020-06-02 16:04:46 -0700

    Updates

---
+++ precache-manifest.1173e9f56a98202f50f8d150cf97bac7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0d9c034b39ab1c27e1c22ddd60db13",
+    "url": "/netflix-app-lists/index.html"
+  },
+  {
+    "revision": "d195854a4f1f450027a5",
+    "url": "/netflix-app-lists/static/css/main.06dba139.chunk.css"
+  },
+  {
+    "revision": "e51b5152c5632ba9f1d5",
+    "url": "/netflix-app-lists/static/js/2.72240886.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/netflix-app-lists/static/js/2.72240886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d195854a4f1f450027a5",
+    "url": "/netflix-app-lists/static/js/main.f8b8ac54.chunk.js"
+  },
+  {
+    "revision": "ef32d29b4f9f02969460",
+    "url": "/netflix-app-lists/static/js/runtime-main.37ee97ac.js"
+  },
+  {
+    "revision": "f0fb0ac07632a41360834be82215505b",
+    "url": "/netflix-app-lists/static/media/Netflix-Logo.f0fb0ac0.png"
+  }
+]);
