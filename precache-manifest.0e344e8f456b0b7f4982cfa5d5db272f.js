bba279a38c789cda4c2eec89d39eb572a30becd4
Jackie <jackielireact@gmail.com>
2020-06-02 16:39:38 -0700

    Updates

---
+++ precache-manifest.0e344e8f456b0b7f4982cfa5d5db272f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1618b39eee2e54275dab3224d0c9b7b8",
+    "url": "/netflix_list_app/index.html"
+  },
+  {
+    "revision": "2f33d49fa690f0b257fc",
+    "url": "/netflix_list_app/static/css/main.06dba139.chunk.css"
+  },
+  {
+    "revision": "2bde7195d7ce144060da",
+    "url": "/netflix_list_app/static/js/2.69211240.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/netflix_list_app/static/js/2.69211240.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f33d49fa690f0b257fc",
+    "url": "/netflix_list_app/static/js/main.2624447c.chunk.js"
+  },
+  {
+    "revision": "0b5ee6887e3897600918",
+    "url": "/netflix_list_app/static/js/runtime-main.be06631c.js"
+  },
+  {
+    "revision": "f0fb0ac07632a41360834be82215505b",
+    "url": "/netflix_list_app/static/media/Netflix-Logo.f0fb0ac0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/netflix_lists_app/precache-manifest.f5dec3f3ce8c024dd8f722f7f72d521f.js"
+  "/netflix_list_app/precache-manifest.0e344e8f456b0b7f4982cfa5d5db272f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/netflix_lists_app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/netflix_list_app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
